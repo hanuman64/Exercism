eede2555d4ec5aeda4811fc41667e4cb7974109d
Pierre Degail <pierro.alex64@gmail.com>
2019-11-14 16:47:23 +0100

    Hamming Changes

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -2,8 +2,26 @@ using System;
 
 public static class Hamming
 {
+    //public static string firstStrand;
+    //public static string secondStrand;
+    //static int lengthFirst = firstStrand.Length;
+    //static int lengthSecond = secondStrand.Length;
+
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (String.IsNullOrEmpty(firstStrand))
+            return 0;
+        if (firstStrand.Length != secondStrand.Length)
+            throw new ArgumentException("length suks");
+        int dist = 0;
+        for(int i = 0; i < firstStrand.Length; i++)
+            {
+              Console.WriteLine(firstStrand[i]);  
+              Console.WriteLine(secondStrand[i]);  
+             if(firstStrand[i] != secondStrand[i])
+                dist++;
+            }
+        return dist;
     }
+
 }
